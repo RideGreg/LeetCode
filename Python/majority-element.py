4529b756287ea47e2e701a5118b0791577aa2fca
kamyu <kamyu104@users.noreply.github.com>
2019-01-06 22:54:44 +0800

    Merge pull request #19 from sangheestyle/master
    add another solution

--- Python/majority-element.py
+++ Python/majority-element.py
@@ -34,5 +34,12 @@ class Solution:
         """
         return sorted(collections.Counter(nums).items(), key=lambda a: a[1], reverse=True)[0][0]
 
+    def majorityElement3(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return collections.Counter(nums).most_common(1)[0][0]
+
 if __name__ == "__main__":
     print Solution().majorityElement([1, 2, 3, 4, 5, 5, 5, 5, 5, 5, 6])
