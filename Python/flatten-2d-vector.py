6d97db71683a43db1a9fb2abe1f152750c397496
Ming Ming <mming@mz.com>
2019-01-09 16:57:17 -0800

    Ming update solution

--- Python/flatten-2d-vector.py
+++ Python/flatten-2d-vector.py
@@ -1,6 +1,29 @@
 # Time:  O(1)
 # Space: O(1)
 
+# 251
+# Implement an iterator to flatten a 2d vector.
+#
+# For example,
+# Given 2d vector =
+# [
+#   [1,2],
+#   [3],
+#   [4,5,6]
+# ]
+#
+#
+# By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,2,3,4,5,6].
+#
+# Hint:
+# 1 How many variables do you need to keep track?
+# 2 Two variables is all you need. Try with x and y.
+# 3 Beware of empty rows. It could be the first few rows.
+# 4 To write correct code, think about the invariant to maintain. What is it?
+# 5 The invariant is x and y must always point to a valid point in the 2d vector. Should you maintain your invariant ahead of time or right when you need it?
+# 6 Not sure? Think about how you would implement hasNext(). Which is more complex?
+# 7 Common logic in two different places should be refactored into a common method.
+
 class Vector2D:
     x, y = 0, 0
     vec = None
@@ -30,3 +53,19 @@ class Vector2D:
             self.x += 1
             if self.x != len(self.vec):
                 self.y = 0
+
+    # add a requirement to remove the element just outputted
+    def remove(self):
+        # case 1: if the element to remove is the last element of the row
+        if self.y == 0:
+            lastRow = self.x - 1
+            while lastRow >= 0 and len(self.vec[lastRow]) == 0:
+                lastRow -= 1
+            lastCol = len(self.vec[lastRow]) - 1
+        else:  # case 2: if the element to remove is not the last element
+            lastCol = self.y - 1
+            lastRow = self.x
+            self.y -= 1
+
+        # remove the element
+        self.vec[lastRow].pop(lastCol)
--- Python/redundant-connection-ii.py
+++ Python/redundant-connection-ii.py
@@ -1,6 +1,7 @@
 # Time:  O(nlog*n) ~= O(n), n is the length of the positions
 # Space: O(n)
 
+# 685 有向图去环去多重父节点
 # In this problem, a rooted tree is a directed graph such that,
 # there is exactly one node (the root) for
 # which all other nodes are descendants of this node, plus every node has exactly one parent,
@@ -53,6 +54,11 @@ class UnionFind(object):
         self.set[min(x_root, y_root)] = max(x_root, y_root)
         return True
 
+# let's enumerate the possibilities for the "redundant" edge.
+# - If there is no loop, then one vertex must have two parents: should return 2nd edge.
+# - If there is a loop, then:
+#   - either one vertex has two parents: don't use 2nd edge in loop testing, then the redundant must be 1st edge;
+#   - or every vertex has one parent: return the last edge formed the cycle.
 
 class Solution(object):
     def findRedundantDirectedConnection(self, edges):
@@ -66,14 +72,62 @@ class Solution(object):
             if edge[1] not in parent:
                 parent[edge[1]] = edge[0]
             else:
+                # record multi-parents
                 cand1 = [parent[edge[1]], edge[1]]
                 cand2 = edge
 
         union_find = UnionFind(len(edges)+1)
         for edge in edges:
-            if edge == cand2:
+            if edge == cand2: # skip cand2 in testing loop, otherwise fail for [[1,2],[1,3],[2,3]], expect [2,3]
                 continue
             if not union_find.union_set(*edge):
-                return cand1 if cand2 else edge
-        return cand2
+                if cand2:
+                    # has both multi parents and loop, return 1st edge in multi parents, since cand2 is not used to test loop
+                    return cand1
+                else:
+                    # no multi parents, return the last edge in loop
+                    return edge
+        return cand2 # no loop, return 2nd edge in multi parents
+
+    # LeetCode official solution
+    def findRedundantDirectedConnection_dfs(self, edges):
+        N = len(edges)
+        parent = {}
+        candidates = []
+        for u, v in edges:
+            if v in parent:
+                candidates.append((parent[v], v))
+                candidates.append((u, v))
+            else:
+                parent[v] = u
+
+        def orbit(node):
+            seen = set()
+            while node in parent and node not in seen:
+                seen.add(node)
+                node = parent[node]
+            return node, seen
+
+        root = orbit(1)[0]
+
+        if not candidates:
+            cycle = orbit(root)[1]
+            for u, v in edges:
+                if u in cycle and v in cycle:
+                    ans = u, v
+            return ans
+
+        children = collections.defaultdict(list)
+        for v in parent:
+            children[parent[v]].append(v)
+
+        seen = [True] + [False] * N
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if not seen[node]:
+                seen[node] = True
+                stack.extend(children[node])
+
+        return candidates[all(seen)]
 
