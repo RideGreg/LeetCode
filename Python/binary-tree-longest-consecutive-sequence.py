542b54f9b0206b072f1563a5e58524839b84949d
kamyu <kamyu104@users.noreply.github.com>
2019-01-15 09:41:08 +0800

    Merge pull request #34 from sangheestyle/binary-tree-longest-consecutive-sequence.py
    update binary-tree-longest-consecutive-sequence.py

--- Python/binary-tree-longest-consecutive-sequence.py
+++ Python/binary-tree-longest-consecutive-sequence.py
@@ -30,7 +30,7 @@ class Solution(object):
             if root.right and root.right.val == root.val + 1:
                 cur_len = max(cur_len, right_len + 1)
 
-            self.max_len = max(self.max_len, cur_len, left_len, right_len)
+            self.max_len = max(self.max_len, cur_len)
 
             return cur_len
 
