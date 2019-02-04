f5c19f659724d87a0d2024ba33f3e82103b488fd
kamyu <kamyu104@users.noreply.github.com>
2019-02-04 13:39:10 +0800

    Merge pull request #63 from sangheestyle/candy.py
    update candy.py

--- Python/candy.py
+++ Python/candy.py
@@ -10,9 +10,7 @@
 # What is the minimum candies you must give?
 #
 
-import operator
-
-class Solution:
+class Solution(object):
     # @param ratings, a list of integer
     # @return an integer
     def candy(self, ratings):
@@ -25,7 +23,7 @@ class Solution:
             if ratings[i - 1] > ratings[i] and candies[i - 1] <= candies[i]:
                 candies[i - 1] = candies[i] + 1
 
-        return reduce(operator.add, candies)
+        return sum(candies)
 
 if __name__ == "__main__":
     result = Solution().candy([1, 2, 3, 2, 3, 5, 2, 5])
