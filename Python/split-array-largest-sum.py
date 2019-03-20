350e6aebbc4409059cba70e548df4637bb0d035e
kamyu <kamyu104@users.noreply.github.com>
2019-03-20 13:29:19 +0800

    Update split-array-largest-sum.py

--- Python/split-array-largest-sum.py
+++ Python/split-array-largest-sum.py
@@ -1,6 +1,7 @@
 # Time:  O(nlogs), s is the sum of nums
 # Space: O(1)
 
+# 410
 # Given an array which consists of non-negative integers and an integer m,
 # you can split the array into m non-empty continuous subarrays.
 # Write an algorithm to minimize the largest sum among these m subarrays.
@@ -22,6 +23,7 @@
 # The best way is to split it into [7,2,5] and [10,8],
 # where the largest sum among the two subarrays is only 18.
 
+# Minimize the largest subarray sum.
 class Solution(object):
     def splitArray(self, nums, m):
         """
@@ -39,16 +41,45 @@ class Solution(object):
                 curr_sum += num
             return True
 
-        left, right = 0, 0
-        for num in nums:
-            left = max(left, num)
-            right += num
-        left = max(left, int(math.ceil(right/m))) #optimization, search range low end is max(largest item, subarray average)
 
-        while left < right:
-            mid = left + (right - left) / 2;
+        left, right = max(nums), sum(nums)
+        left = max(left, int(math.ceil(right/m))) #optimization, search range low end is max(largest item, subarray average)
+        while left <= right:
+            mid = left + (right - left) / 2
+
             if canSplit(nums, m, mid):
                 right = mid
+            else:
+                left = mid + 1
+        return left
+
+
+# Time:  O(nlogs), s is the sum of nums
+# Space: O(1)
+
+# Maximize the smallest subarray sum.
+class SolutionFindMaxMin(object):
+    def splitArray(self, nums, m):
+        """
+        :type nums: List[int]
+        :type m: int
+        :rtype: int
+        """
+        def canSplit(nums, m, s):
+            cnt, curr_sum = 1, 0
+            for num in nums:
+                curr_sum += num
+                if curr_sum > s:
+                    curr_sum = 0
+                    cnt += 1
+            return cnt <= m
+
+        left, right = min(nums), sum(nums)
+        while left <= right:
+            mid = left + (right - left) / 2
+            x = canSplit(nums, m, mid)
+            if x:
+                right = mid - 1
             else:
                 left = mid + 1
         return left
