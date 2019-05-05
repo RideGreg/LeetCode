6421581ebbb3a74e4c7b3781df4e315f161172c6
kamyu <kamyu104@users.noreply.github.com>
2019-05-05 16:09:12 +0800

    Update moving-stones-until-consecutive.py

--- Python/moving-stones-until-consecutive.py
+++ Python/moving-stones-until-consecutive.py
@@ -32,3 +32,24 @@ class Solution(object):
         else:
             mn = 2
         return [mn, mx]
+
+
+# Time:  O(1)
+# Space: O(1)
+class Solution2(object):
+    def numMovesStones(self, a, b, c):
+        """
+        :type a: int
+        :type b: int
+        :type c: int
+        :rtype: List[int]
+        """
+        stones = [a, b, c]
+        stones.sort()
+        left, min_moves = 0, float("inf")
+        max_moves = (stones[-1]-stones[0]) - (len(stones)-1)
+        for right in range(len(stones)):
+            while stones[right]-stones[left]+1 > len(stones): # find window size <= len(stones)
+                left += 1
+            min_moves = min(min_moves, len(stones)-(right-left+1))  # move stones not in this window
+        return [min_moves, max_moves]
