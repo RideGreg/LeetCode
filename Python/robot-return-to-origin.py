96e61de9a947387b9e2c45c94e3e118bd1e5998b
kamyu <kamyu104@users.noreply.github.com>
2019-08-16 12:54:06 +0800

    Update robot-return-to-origin.py

--- Python/robot-return-to-origin.py
+++ Python/robot-return-to-origin.py
@@ -15,8 +15,20 @@
 # Input: "LL"
 # Output: false
 
+import collections
+
+
 class Solution(object):
     def judgeCircle(self, moves):
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        count = collections.Counter(moves)
+        return count['L'] == count['R'] and count['U'] == count['D']
+
+ 
+    def judgeCircle2(self, moves):
         """
         :type moves: str
         :rtype: bool
