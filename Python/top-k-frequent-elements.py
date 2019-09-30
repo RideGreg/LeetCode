kamyu104/LeetCode-Solutions

Update top-k-frequent-elements.py

Python/top-k-frequent-elements.py
@@ -40,7 +40,7 @@ class Solution2(object):
         p = []
         for key, val in counts.iteritems():
             p.append((-val, key))
-        self.kthElement(p, k)
+        self.kthElement(p, k-1)
 
         result = []
         for i in xrange(k):
@@ -64,11 +64,11 @@ class Solution2(object):
         while left <= right:
             pivot_idx = randint(left, right)
             new_pivot_idx = PartitionAroundPivot(left, right, pivot_idx, nums)
-            if new_pivot_idx == k - 1:
+            if new_pivot_idx == k:
                 return
-            elif new_pivot_idx > k - 1:
+            elif new_pivot_idx > k:
                 right = new_pivot_idx - 1
-            else:  # new_pivot_idx < k - 1.
+            else:  # new_pivot_idx < k.
                 left = new_pivot_idx + 1
 
 

RideGreg/LeetCode

Python/top-k-frequent-elements.py
@@ -60,6 +60,7 @@ class Solution2(object):
         :type k: int
         :rtype: List[int]
         """
+<<<<<<< HEAD
         def kthElement(A, k):
             def partition(l, r, pivot):
                 new_pivot = l
@@ -76,9 +77,9 @@ class Solution2(object):
             while l <= r:
                 pivot = random.randint(l, r)
                 new_pivot = partition(l, r, pivot)
-                if new_pivot == k - 1:
+                if new_pivot == k:
                     return
-                elif new_pivot > k - 1:
+                elif new_pivot > k:
                     r = new_pivot - 1
                 else:
                     l = new_pivot + 1
@@ -87,7 +88,7 @@ class Solution2(object):
         pairs = []
         for n, count in counts.items():
             pairs.append((-count, n))
-        kthElement(pairs, k)
+        kthElement(pairs, k-1)
         return [x[1] for x in pairs[:k]]
 
 # Time:  O(nlogk)