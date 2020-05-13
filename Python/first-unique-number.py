kamyu104/LeetCode-Solutions

Create first-unique-number.py

Python/first-unique-number.py
@@ -1,0 +1,40 @@
+# Time:  ctor: O(k)
+#        add: O(1)
+#        showFirstUnique: O(1)
+# Space: O(n)
+
+import collections
+
+
+class FirstUnique(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.__q = collections.OrderedDict()
+        self.__dup = set()
+        for num in nums:
+            self.add(num)
+
+    def showFirstUnique(self):
+        """
+        :rtype: int
+        """
+        if self.__q:
+            return next(iter(self.__q))
+        return -1
+    
+
+    def add(self, value):
+        """
+        :type value: int
+        :rtype: None
+        """
+        if value not in self.__dup and value not in self.__q:
+            self.__q[value] = None
+            return
+        if value in self.__q:
+            self.__q.pop(value)
+            self.__dup.add(value)
+

RideGreg/LeetCode

Python/first-unique-number.py
@@ -1,4 +1,9 @@
+# Time:  ctor: O(k)
+#        add: O(1)
+#        showFirstUnique: O(1)
+# Space: O(n)
 
+# 1429
 # You have a queue of integers, you need to retrieve the first unique integer in the queue.
 #
 # Implement the FirstUnique class:
@@ -7,6 +12,59 @@
 #   void add(int value) insert value to the queue.
 
 from typing import List
+import collections
+
+
+class FirstUnique(object): # USE THIS: use OrderedDict to controll order, actually only need OrderedSet (a special case of OrderedDict)
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.__unique = collections.OrderedDict()
+        self.__dup = set()
+        for num in nums:
+            self.add(num)
+
+    def showFirstUnique(self):
+        """
+        :rtype: int
+        """
+        if self.__unique:
+            return next(iter(self.__unique))
+        return -1
+    
+
+    def add(self, value):
+        """
+        :type value: int
+        :rtype: None
+        """
+        if value not in self.__dup and value not in self.__unique:
+            self.__unique[value] = None # only need to save key, value can be None or 1 or anything
+            return
+        if value in self.__unique:
+            self.__unique.pop(value) # same effect as del self.__unique[value]
+            self.__dup.add(value)
+
+
+class FirstUnique2: # similar to method 1, but inferior by storing both unique and dup in Counter
+    def __init__(self, nums: List[int]):
+        self.unique = collections.OrderedDict()
+        self.cnt = collections.Counter([])
+        for n in nums:
+            self.add(n)
+
+    def showFirstUnique(self) -> int:
+        return next(iter(self.unique)) if self.unique else -1
+
+    def add(self, value: int) -> None:
+        self.cnt[value] += 1
+        if self.cnt[value] == 1:
+            self.unique[value] = 1
+        elif value in self.unique:
+            del self.unique[value]
+
 
 # doubly linked list to store nodes with unique numbers. dict to store the mapping
 # from value to: None (repeating value), list node (unique value), not in dict (value not seen).
@@ -15,7 +73,7 @@ class DDLNode(object):
         self.v = x
         self.prev = None
         self.next = None
-class FirstUnique:
+class FirstUnique3:
     def __init__(self, nums: List[int]):
         self.head = self.tail = None
         self.lookup = {}
@@ -51,23 +109,6 @@ class FirstUnique:
             del node
             self.lookup[value] = None
 
-import collections
-class FirstUnique2: # USE THIS: use OrderedDict to controll order, actually only need OrderedSet (a special case of OrderedDict)
-    def __init__(self, nums: List[int]):
-        self.unique = collections.OrderedDict()
-        self.cnt = collections.Counter([])
-        for n in nums:
-            self.add(n)
-
-    def showFirstUnique(self) -> int:
-        return next(iter(self.unique)) if self.unique else -1
-
-    def add(self, value: int) -> None:
-        self.cnt[value] += 1
-        if self.cnt[value] == 1:
-            self.unique[value] = 1
-        elif value in self.unique:
-            del self.unique[value]
 
 obj = FirstUnique([2,3,5])
 print(obj.showFirstUnique()) # 2
@@ -84,3 +125,7 @@ print("\n")
 obj = FirstUnique([809])
 print(obj.showFirstUnique()) # 809
 obj.add(809); print(obj.showFirstUnique()) # -1
+=======
+
+
+>>>>>>> d9628635... Create first-unique-number.py