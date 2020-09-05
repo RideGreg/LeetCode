3189c8675a27fc424e9b8ad1fb6d7457a7ddb08d
kamyu <kamyu104@users.noreply.github.com>
2020-09-06 00:13:36 +0800

    Update word-ladder.py

--- Python/word-ladder.py
+++ Python/word-ladder.py
@@ -49,6 +49,69 @@ class Solution(object):
         return 0
 
 
+# Time:  O(26 * d * l) = O(d * l), d is the size of wordlist, l is the max length of words
+# Space: O(d * l)
+
+from string import ascii_lowercase
+
+
+# two-end bfs
+class Solution2(object):
+    def ladderLength(self, beginWord, endWord, wordList):
+        lookup = set(wordList)
+        if endWord not in lookup:
+            return 0
+        ladder = 2
+        left, right = set([beginWord]), set([endWord])
+        while left and right:
+            if len(left) > len(right):
+                left, right = right, left
+            new_left = []
+            for word in left:
+                for i in xrange(len(word)):
+                    for j in ascii_lowercase:
+                        new_word = word[:i] + j + word[i+1:]
+                        if new_word in right:
+                            return ladder
+                        if new_word in lookup:
+                            lookup.remove(new_word)
+                            new_left.append(new_word)
+            left = new_left
+            ladder += 1
+        return 0
+
+
+# Time:  O(26 * d * l) = O(d * l), d is the size of wordlist, l is the max length of words
+# Space: O(d * l)
+class Solution3(object):
+    def ladderLength(self, beginWord, endWord, wordList):
+        """
+        :type beginWord: str
+        :type endWord: str
+        :type wordList: List[str]
+        :rtype: int
+        """
+        lookup = set(wordList)
+        if endWord not in lookup:
+            return 0
+        ladder = 2
+        q = [beginWord]
+        while q:
+            new_q = []
+            for word in q:
+                for i in xrange(len(word)):
+                    for j in ascii_lowercase:
+                        new_word = word[:i] + j + word[i+1:]
+                        if new_word == endWord:
+                            return ladder
+                        if new_word in lookup:
+                            lookup.remove(new_word)
+                            new_q.append(new_word)
+            q = new_q
+            ladder += 1
+        return 0
+
+
 if __name__ == "__main__":
     print(Solution().ladderLength("hit", "cog", set(["hot", "dot", "dog", "lot", "log"])))
     print(Solution().ladderLength("hit", "cog", set(["hot", "dot", "dog", "lot", "log", "cog"])))
