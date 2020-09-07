f1b56e5ec52af7bc7b09b2c7736846280c329570
kamyu <kamyu104@users.noreply.github.com>
2020-09-07 22:46:24 +0800

    Update decode-string.py

--- Python/decode-string.py
+++ Python/decode-string.py
@@ -114,18 +114,16 @@ class Solution(object):
         for c in s:
             if c.isdigit():
                 n = n * 10 + ord(c) - ord('0')
+            elif c.isalpha():
+                curr.append(c)
             elif c == '[':
                 nums.append(n)
-                n = 0
                 strs.append(curr)
-                curr = []
+                n, curr = 0, []
             elif c == ']':
                 strs[-1].extend(curr * nums.pop())
                 curr = strs.pop()
-            else:
-                curr.append(c)
-
-        return "".join(strs[-1]) if strs else "".join(curr)
+        return "".join(curr)
 
 print(Solution().decodeString("3[a]2[bc]")) # "aaabcbc"
 print(Solution().decodeString("3[a2[c]]")) # "accaccacc"
