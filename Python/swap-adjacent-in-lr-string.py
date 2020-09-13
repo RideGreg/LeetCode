be3fcc3eb41250b630fe1a2e135cbb2d61837eb3
kamyu <kamyu104@users.noreply.github.com>
2020-09-13 18:24:18 +0800

    Update swap-adjacent-in-lr-string.py

--- Python/swap-adjacent-in-lr-string.py
+++ Python/swap-adjacent-in-lr-string.py
@@ -1,6 +1,7 @@
 # Time:  O(n)
 # Space: O(1)
 
+# 777
 # In a string composed of 'L', 'R', and 'X' characters, like "RXXLRXRXL",
 # a move consists of either replacing one occurrence of "XL" with "LX",
 # or replacing one occurrence of "RX" with "XR".
@@ -23,6 +24,13 @@
 # - 1 <= len(start) = len(end) <= 10000.
 # - Both start and end will only consist of characters in {'L', 'R', 'X'}.
 
+
+# Analysis
+# the followings are invariant if the number of 'X' in both strings are the same
+# 1. the ordering of 'L', 'R' in both strings are the same
+# 2. for each position (i, j) of paired 'L' character in both strings, i >= j
+# 3. for each position (i, j) of paired 'R' character in both strings, i <= j
+
 class Solution(object):
     def canTransform(self, start, end):
         """
@@ -30,16 +38,17 @@ class Solution(object):
         :type end: str
         :rtype: bool
         """
-        N = len(start)
+        if start.count('X') != end.count('X'):
+            return False
         i, j = 0, 0
-        while i < N and j < N:
-            while i < N and start[i] == 'X':
+        while i < len(start) and j < len(end):
+            while i < len(start) and start[i] == 'X':
                 i += 1
-            while j < N and end[j] == 'X':
+            while j < len(end) and end[j] == 'X':
                 j += 1
-            if (i < N) != (j < N):
+            if (i < len(start)) != (j < len(end)):
                 return False
-            elif i < N and j < N:
+            elif i < len(start) and j < len(end):
                 if start[i] != end[j] or \
                    (start[i] == 'L' and i < j) or \
                    (start[i] == 'R' and i > j):
