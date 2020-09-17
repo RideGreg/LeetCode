kamyu104/LeetCode-Solutions

Update letter-combinations-of-a-phone-number.py

Python/letter-combinations-of-a-phone-number.py
@@ -16,7 +16,7 @@ class Solution(object):
         for digit in reversed(digits):
             choices = lookup[int(digit)]
             m, n = len(choices), len(result)
-            result += [result[i % n] for i in xrange(n, m*n)]
+            result.extend([result[i % n] for i in xrange(n, m*n)])
             for i in xrange(m*n):
                 result[i] = choices[i//n] + result[i]
         return result

RideGreg/LeetCode

Python/letter-combinations-of-a-phone-number.py
@@ -54,10 +54,9 @@ class Solution:
         for digit in reversed(digits):
             choices = lookup[int(digit)]
             m, n = len(choices), len(result)
-            result += [result[i % n] for i in range(n, m * n)]
+            result.extend([result[i % n] for i in range(n, m * n)])
             for i in range(m * n):
                 result[i] = choices[i // n] + result[i]
-
         return result
 
 
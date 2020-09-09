719bbd38af0a38dbcf98b7a5fc305077c2f2659e
kamyu <kamyu104@users.noreply.github.com>
2020-09-10 01:24:57 +0800

    Update letter-combinations-of-a-phone-number.py

--- Python/letter-combinations-of-a-phone-number.py
+++ Python/letter-combinations-of-a-phone-number.py
@@ -54,10 +54,9 @@ class Solution:
         for digit in reversed(digits):
             choices = lookup[int(digit)]
             m, n = len(choices), len(result)
-            result += [result[i % n] for i in range(n, m * n)]
+            result.extend([result[i % n] for i in range(n, m * n)])
             for i in range(m * n):
                 result[i] = choices[i // n] + result[i]
-
         return result
 
 
