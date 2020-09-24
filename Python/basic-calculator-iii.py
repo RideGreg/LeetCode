420027137a2ecd990a7eeea0248df4dc8b25aa5f
kamyu <kamyu104@users.noreply.github.com>
2020-09-24 22:06:00 +0800

    Update basic-calculator-iii.py

--- Python/basic-calculator-iii.py
+++ Python/basic-calculator-iii.py
@@ -25,8 +25,47 @@ try:
 except NameError:
     xrange = range  # Python 3
 
+import operator
 
-class Solution(object):
+
+class Solution(object): # dont use this: too fancy, prefer simpleness and easy to understand
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        def compute(operands, operators):
+            right, left = operands.pop(), operands.pop()
+            operands.append(ops[operators.pop()](left, right))
+
+        ops = {'+':operator.add, '-':operator.sub, '*':operator.mul, '/':operator.div}
+        precedence = {'+':0, '-':0, '*':1, '/':1}
+        operands, operators, operand = [], [], 0
+        for i in xrange(len(s)):
+            if s[i].isdigit():
+                operand = operand*10 + int(s[i])
+                if i == len(s)-1 or not s[i+1].isdigit():
+                    operands.append(operand)
+                    operand = 0
+            elif s[i] == '(':
+                operators.append(s[i])
+            elif s[i] == ')':
+                while operators and operators[-1] != '(':
+                    compute(operands, operators)
+                operators.pop()
+            elif s[i] in precedence:
+                while operators and operators[-1] in precedence and \
+                      precedence[operators[-1]] >= precedence[s[i]]:
+                    compute(operands, operators)
+                operators.append(s[i])
+        while operators:
+            compute(operands, operators)
+        return operands[-1]
+
+
+# Time:  O(n)
+# Space: O(n)
+class Solution2(object): # USE THIS
     def calculate(self, s):
         """
         :type s: str
