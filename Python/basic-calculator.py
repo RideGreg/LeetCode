79618dc5d8a35afb0a7384003a82c0399ffa79e4
kamyu <kamyu104@users.noreply.github.com>
2020-09-24 23:43:55 +0800

    Update basic-calculator.py

--- Python/basic-calculator.py
+++ Python/basic-calculator.py
@@ -26,11 +26,52 @@ except NameError:
 # If forward scan the input, we end up calculate from right to left, not easy to handle subtraction '(A-B)-C'.
 # so we scan from right to left, and calculate starting from left.
 
+import operator
+
+
+class Solution(object):
+    def calculate(self, s):  # prefer not to use, fancy/general but not simple code
+        """
+        :type s: str
+        :rtype: int
+        """
+        def compute(operands, operators):
+            right, left = operands.pop(), operands.pop()
+            operands.append(ops[operators.pop()](left, right))
+
+        ops = {'+':operator.add, '-':operator.sub, '*':operator.mul, '/':operator.div}
+        precedence = {'+':0, '-':0, '*':1, '/':1}
+        operands, operators, operand = [], [], 0
+        for i in xrange(len(s)):
+            if s[i].isdigit():
+                operand = operand*10 + int(s[i])
+                if i == len(s)-1 or not s[i+1].isdigit():
+                    operands.append(operand)
+                    operand = 0
+            elif s[i] == '(':
+                operators.append(s[i])
+            elif s[i] == ')':
+                while operators[-1] != '(':
+                    compute(operands, operators)
+                operators.pop()
+            elif s[i] in precedence:
+                while operators and operators[-1] in precedence and \
+                      precedence[operators[-1]] >= precedence[s[i]]:
+                    compute(operands, operators)
+                operators.append(s[i])
+        while operators:
+            compute(operands, operators)
+        return operands[-1]
+
+
+# Time:  O(n)
+# Space: O(n)
+
 # Edge case: starting with '-'
-class Solution:
+class Solution2(object):
     # @param {string} s
     # @return {integer}
-    def calculate(self, s):
+    def calculate(self, s):  # USE THIS
         operands, operators = [], []
         if s.lstrip()[0] == '-': s = '0'+s
         s = '#'+s
