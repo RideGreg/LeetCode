96910afeacbe9afd47b96cea1ed49ecb9cadaaa1
kamyu <kamyu104@users.noreply.github.com>
2020-10-26 15:04:38 +0800

    Update 01-matrix.py

--- Python/01-matrix.py
+++ Python/01-matrix.py
@@ -1,5 +1,5 @@
 # Time:  O(m * n)
-# Space: O(m * n)
+# Space: O(1)
 
 # Given a matrix consists of 0 and 1, find the distance of the nearest 0 for each cell.
 # The distance between two adjacent cells is 1.
@@ -41,13 +41,45 @@ except NameError:
     xrange = range  # Python 3
 
 
+# dp solution
 class Solution(object):
     def updateMatrix(self, matrix):
         """
         :type matrix: List[List[int]]
         :rtype: List[List[int]]
         """
-        queue = collections.deque([])
+        for i in xrange(len(matrix)):
+            for j in xrange(len(matrix[i])):
+                if not matrix[i][j]:
+                    continue
+                matrix[i][j] = float("inf")
+                if i > 0:
+                    matrix[i][j] = min(matrix[i][j], matrix[i-1][j]+1)
+                if j > 0:
+                    matrix[i][j] = min(matrix[i][j], matrix[i][j-1]+1)
+        for i in reversed(xrange(len(matrix))):
+            for j in reversed(xrange(len(matrix[i]))):
+                if not matrix[i][j]:
+                    continue
+                if i < len(matrix)-1:
+                    matrix[i][j] = min(matrix[i][j], matrix[i+1][j]+1)
+                if j < len(matrix[i])-1:
+                    matrix[i][j] = min(matrix[i][j], matrix[i][j+1]+1)
+        return matrix
+
+
+# Time:  O(m * n)
+# Space: O(m * n)
+import collections
+
+
+class Solution3(object):
+    def updateMatrix(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        queue = collections.deque()
         for i in xrange(len(matrix)):
             for j in xrange(len(matrix[0])):
                 if matrix[i][j] == 0:
@@ -68,34 +100,3 @@ class Solution(object):
                 matrix[i][j] = matrix[cell[0]][cell[1]]+1
 
         return matrix
-
-
-# Time:  O(m * n)
-# Space: O(m * n)
-# dp solution
-class Solution2(object):
-    def updateMatrix(self, matrix):
-        """
-        :type matrix: List[List[int]]
-        :rtype: List[List[int]]
-        """
-        dp = [[float("inf")]*len(matrix[0]) for _ in xrange(len(matrix))]
-        for i in xrange(len(matrix)):
-            for j in xrange(len(matrix[i])):
-                if matrix[i][j] == 0:
-                    dp[i][j] = 0
-                else:
-                    if i > 0:
-                        dp[i][j] = min(dp[i][j], dp[i-1][j]+1)
-                    if j > 0:
-                        dp[i][j] = min(dp[i][j], dp[i][j-1]+1)
-        for i in reversed(xrange(len(matrix))):
-            for j in reversed(xrange(len(matrix[i]))):
-                if matrix[i][j] == 0:
-                    dp[i][j] = 0
-                else:
-                    if i < len(matrix)-1:
-                        dp[i][j] = min(dp[i][j], dp[i+1][j]+1)
-                    if j < len(matrix[i])-1:
-                        dp[i][j] = min(dp[i][j], dp[i][j+1]+1)
-        return dp
