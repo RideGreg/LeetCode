8462d4c03b52aa1387967016f1a39d64bcbe8c1b
kamyu <kamyu104@users.noreply.github.com>
2020-10-26 15:41:50 +0800

    Update shortest-palindrome.py

--- Python/shortest-palindrome.py
+++ Python/shortest-palindrome.py
@@ -1,6 +1,7 @@
 # Time:  O(n)
 # Space: O(n)
-#
+
+# 214
 # Given a string S, you are allowed to convert it to a palindrome
 # by adding characters in front of it. Find and return the shortest
 # palindrome you can find by performing this transformation.
@@ -13,6 +14,7 @@
 #
 
 # KMP Algorithm
+# optimized from Solution2
 class Solution(object):
     def shortestPalindrome(self, s):
         """
@@ -22,7 +24,33 @@ class Solution(object):
         def getPrefix(pattern):
             prefix = [-1] * len(pattern)
             j = -1
-            for i in xrange(1, len(pattern)):
+            for i in range(1, len(pattern)):
+                while j > -1 and pattern[j+1] != pattern[i]:
+                    j = prefix[j]
+                if pattern[j+1] == pattern[i]:
+                    j += 1
+                prefix[i] = j
+            return prefix
+
+        if not s:
+            return s
+
+        A = s + '#' + s[::-1]
+        return s[getPrefix(A)[-1]+1:][::-1] + s
+
+
+# Time:  O(n)
+# Space: O(n)
+class Solution2(object):
+    def shortestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        def getPrefix(pattern):
+            prefix = [-1] * len(pattern)
+            j = -1
+            for i in range(1, len(pattern)):
                 while j > -1 and pattern[j+1] != pattern[i]:
                     j = prefix[j]
                 if pattern[j+1] == pattern[i]:
@@ -44,7 +72,7 @@ class Solution(object):
 # Time:  O(n)
 # Space: O(n)
 # Manacher's Algorithm
-class Solution2(object):
+class Solution3(object):
     def shortestPalindrome(self, s):
         """
         :type s: str
@@ -62,7 +90,7 @@ class Solution2(object):
         string = preProcess(s)
         palindrome = [0] * len(string)
         center, right = 0, 0
-        for i in xrange(1, len(string) - 1):
+        for i in range(1, len(string) - 1):
             i_mirror = 2 * center - i
             if right > i:
                 palindrome[i] = min(right - i, palindrome[i_mirror])
@@ -76,7 +104,7 @@ class Solution2(object):
                 center, right = i, i + palindrome[i]
 
         max_len = 0
-        for i in xrange(1, len(string) - 1):
+        for i in range(1, len(string) - 1):
             if i - palindrome[i] == 1:
                 max_len = palindrome[i]
         return s[len(s)-1:max_len-1:-1] + s
@@ -88,11 +116,12 @@ class Solution2(object):
 class Solution4(object):
     def shortestPalindrome(self, s):
         rev = s[::-1]
-        for i in xrange(len(s)):
+        for i in range(len(s)):
             if s[:len(s)-i] == rev[i:]:
                 return rev[:i] + s
         return ''
 
+    # each iteration has to make a new substring
     def shortestPalindrome_TLE(self, s):
         def isPalindrome(s):
             if len(s) < 2:
@@ -105,7 +134,10 @@ class Solution4(object):
                 j -= 1
             return True
 
-        for i in xrange(len(s)):
+        for i in range(len(s)):
             if isPalindrome(s[:len(s) - i]):
                 return s[len(s) - i:][::-1] + s
         return ''
+
+print(Solution().shortestPalindrome("abcd")) # "dcbabcd"
+print(Solution().shortestPalindrome("aacecaaa")) # "aaacecaaa"
